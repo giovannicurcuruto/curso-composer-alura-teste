daf63fc08d3ec50d77397b774c3d80763bd7c2ef
Giovanni Curcuruto <ggcurcuruto@gmail.com>
2021-06-15 21:46:56 -0300

    Initial commit

---
+++ Teste-busca.php
@@ -0,0 +1,19 @@
+<?php
+
+class Teste
+{
+    public static function metodo()
+    {
+        echo "Teste". PHP_EOL;;
+    }
+
+}
+
+class Teste2
+{
+    public static function metodo2()
+    {
+        echo "Teste2" . PHP_EOL;
+    }
+
+}
---
+++ buscar-cursos.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'vendor/autoload.php';
+
+//Teste::metodo();
+//Teste2::metodo2();
+
+//exit();
+
+
+use Alura\BuscadorDeCursos\Buscador;
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
+$client = new Client([
+    'base_uri' => 'https://www.alura.com.br',
+    'verify' => false
+]);
+
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo exibeMensagem($curso);
+}
